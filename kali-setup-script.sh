d23a47fc01449b4849f588dc6a2b996f2b89a3a9
Joel Moore <joel.moore@blacklanternsecurity.com>
2019-12-10 16:33:39 -0500

    eaphammer, cypheroth, eavesarp

--- kali-setup-script.sh
+++ kali-setup-script.sh
@@ -36,33 +36,23 @@ name=*
 LLMNR=no' > /etc/systemd/network/90-disable-llmnr.network
 
 
-printf '\n============================================================\n'
-printf '[+] Disabling Terminal Transparency\n'
-printf '============================================================\n\n'
-profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
-profile=${profile:1:-1}
-gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" use-transparent-background false
-# bring back minimize/maxminize buttons
-gsettings set org.gnome.desktop.wm.preferences button-layout appmenu:minimize,maximize,close
-
-
-printf '\n============================================================\n'
-printf '[+] Removing the abomination that is gnome-software\n'
-printf '============================================================\n\n'
-killall gnome-software
-while true
-do
-    pgrep gnome-software &>/dev/null || break
-    sleep .5
-done
-yes '' | apt-get remove gnome-software
+# printf '\n============================================================\n'
+# printf '[+] Removing the abomination that is gnome-software\n'
+# printf '============================================================\n\n'
+# killall gnome-software
+# while true
+# do
+#     pgrep gnome-software &>/dev/null || break
+#     sleep .5
+# done
+# yes '' | apt-get remove gnome-software
 
 
 printf '\n============================================================\n'
 printf '[+] Setting Theme\n'
 printf '============================================================\n\n'
 # dark theme
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+# gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 mkdir -p '/usr/share/wallpapers/wallpapers/' &>/dev/null
 wallpaper_file="$(find . -type f -name bls_wallpaper.png)"
 if [[ -z "$wallpaper_file" ]]
@@ -77,55 +67,56 @@ gsettings set org.gnome.desktop.background color-shading-type "solid"
 gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/wallpapers/wallpapers/bls_wallpaper.png"
 gsettings set org.gnome.desktop.screensaver picture-uri "file:///usr/share/wallpapers/wallpapers/bls_wallpaper.png"
 gsettings set org.gnome.desktop.background picture-options scaled
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s /usr/share/wallpapers/wallpapers/bls_wallpaper.png
 
 
-printf '\n============================================================\n'
-printf '[+] Installing i3\n'
-printf '============================================================\n\n'
-# install dependencies
-yes '' | apt-get install i3 j4-dmenu-desktop gnome-flashback fonts-hack feh
-cd /opt
-git clone https://github.com/csxr/i3-gnome
-cd i3-gnome
-make install
-# make startup script
-echo '#!/bin/bash
-# xrandr --output eDP-1 --mode 1920x1080
-feh --bg-scale /usr/share/wallpapers/wallpapers/bls_wallpaper.png
-' > /root/.config/i3_startup.sh
-
-# set up config
-grep '### KALI SETUP SCRIPT ###' /etc/i3/config.keycodes || echo '
-### KALI SETUP SCRIPT ###
-# win+L lock screen
-bindsym $sup+l exec i3lock -i /usr/share/wallpapers/wallpapers/bls_wallpaper.png
-# gnome settings daemon
-exec --no-startup-id /usr/lib/gnome-settings-daemon/gsd-xsettings
-# gnome power manager
-exec_always --no-startup-id gnome-power-manager
-# polkit-gnome
-exec --no-startup-id /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
-# gnome flashback
-exec --no-startup-id gnome-flashback
-# resolution / wallpaper
-exec_always --no-startup-id bash "/root/.config/i3_startup.sh"
-
-# BLS theme
-# class             border  background  text        indicator   child_border
-client.focused      #444444 #444444     #FFFFFF     #FFFFFF     #444444
-' >> /etc/i3/config.keycodes
-
-# gnome terminal
-sed -i 's/^bindcode $mod+36 exec.*/bindcode $mod+36 exec gnome-terminal/' /etc/i3/config.keycodes
-# improved dmenu
-sed -i 's/.*bindcode $mod+40 exec.*/bindcode $mod+40 exec --no-startup-id j4-dmenu-desktop/g' /etc/i3/config.keycodes
-# mod+shift+e logs out of gnome
-sed -i 's/.*bindcode $mod+Shift+26 exec.*/bindcode $mod+Shift+26 exec gnome-session-quit/g' /etc/i3/config.keycodes
-# hack font
-sed -i 's/^font pango:.*/font pango:hack 11/' /etc/i3/config.keycodes
-# focus child
-sed -i 's/bindcode $mod+39 layout stacking/#bindcode $mod+39 layout stacking/g' /etc/i3/config.keycodes
-sed -i 's/.*bindsym $mod+d focus child.*/bindcode $mod+39 focus child/g' /etc/i3/config.keycodes
+# printf '\n============================================================\n'
+# printf '[+] Installing i3\n'
+# printf '============================================================\n\n'
+# # install dependencies
+# yes '' | apt-get install i3 j4-dmenu-desktop gnome-flashback fonts-hack feh
+# cd /opt
+# git clone https://github.com/csxr/i3-gnome
+# cd i3-gnome
+# make install
+# # make startup script
+# echo '#!/bin/bash
+# # xrandr --output eDP-1 --mode 1920x1080
+# feh --bg-scale /usr/share/wallpapers/wallpapers/bls_wallpaper.png
+# ' > /root/.config/i3_startup.sh
+# 
+# # set up config
+# grep '### KALI SETUP SCRIPT ###' /etc/i3/config.keycodes || echo '
+# ### KALI SETUP SCRIPT ###
+# # win+L lock screen
+# bindsym $sup+l exec i3lock -i /usr/share/wallpapers/wallpapers/bls_wallpaper.png
+# # gnome settings daemon
+# exec --no-startup-id /usr/lib/gnome-settings-daemon/gsd-xsettings
+# # gnome power manager
+# exec_always --no-startup-id gnome-power-manager
+# # polkit-gnome
+# exec --no-startup-id /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+# # gnome flashback
+# exec --no-startup-id gnome-flashback
+# # resolution / wallpaper
+# exec_always --no-startup-id bash "/root/.config/i3_startup.sh"
+# 
+# # BLS theme
+# # class             border  background  text        indicator   child_border
+# client.focused      #444444 #444444     #FFFFFF     #FFFFFF     #444444
+# ' >> /etc/i3/config.keycodes
+# 
+# # gnome terminal
+# sed -i 's/^bindcode $mod+36 exec.*/bindcode $mod+36 exec gnome-terminal/' /etc/i3/config.keycodes
+# # improved dmenu
+# sed -i 's/.*bindcode $mod+40 exec.*/bindcode $mod+40 exec --no-startup-id j4-dmenu-desktop/g' /etc/i3/config.keycodes
+# # mod+shift+e logs out of gnome
+# sed -i 's/.*bindcode $mod+Shift+26 exec.*/bindcode $mod+Shift+26 exec gnome-session-quit/g' /etc/i3/config.keycodes
+# # hack font
+# sed -i 's/^font pango:.*/font pango:hack 11/' /etc/i3/config.keycodes
+# # focus child
+# sed -i 's/bindcode $mod+39 layout stacking/#bindcode $mod+39 layout stacking/g' /etc/i3/config.keycodes
+# sed -i 's/.*bindsym $mod+d focus child.*/bindcode $mod+39 focus child/g' /etc/i3/config.keycodes
 
 
 printf '\n============================================================\n'
@@ -165,7 +156,8 @@ yes '' | apt-get install \
     remmina \
     nfs-kernel-server \
     dnsmasq \
-    hcxtools
+    hcxtools \
+    gnome-terminal
 python2 -m pip install pipenv
 python3 -m pip install pipenv
 python3 -m pip install mitmproxy
@@ -260,14 +252,26 @@ printf '[+] Installing Bloodhound\n'
 printf '============================================================\n\n'
 # uninstall old version
 yes '' | apt-get remove bloodhound
+rm -rf /opt/BloodHound-linux-x64 &>/dev/null
+
 # download latest bloodhound release from github
 release_url="https://github.com/$(curl -s https://github.com/BloodHoundAD/BloodHound/releases | egrep -o '/BloodHoundAD/BloodHound/releases/download/.{1,10}/BloodHound-linux-x64.zip' | head -n 1)"
 cd /opt
 wget "$release_url"
 unzip -o 'BloodHound-linux-x64.zip'
 rm 'BloodHound-linux-x64.zip'
-ln -s '/opt/BloodHound-linux-x64/BloodHound' '/usr/local/bin/bloodhound'
 
+# fix white screen issue
+echo -e '#!/bin/bash\n/opt/BloodHound-linux-x64/BloodHound --no-sandbox $@' > /usr/local/bin/bloodhound
+chmod +x /usr/local/bin/bloodhound
+
+# install Neo4J
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | apt-key add -
+echo 'deb https://debian.neo4j.org/repo stable/' > /etc/apt/sources.list.d/neo4j.list
+yes '' | apt-get update
+yes '' | apt-get install neo4j
+
+# increase open file limit
 yes '' | apt-get install neo4j gconf-service gconf2-common libgconf-2-4
 mkdir -p /usr/share/neo4j/logs /usr/share/neo4j/run
 grep '^root   soft    nofile' /etc/security/limits.conf || echo 'root   soft    nofile  500000
@@ -275,8 +279,14 @@ root   hard    nofile  600000' >> /etc/security/limits.conf
 grep 'NEO4J_ULIMIT_NOFILE=60000' /etc/default/neo4j 2>/dev/null || echo 'NEO4J_ULIMIT_NOFILE=60000' >> /etc/default/neo4j
 grep 'fs.file-max' /etc/sysctl.conf 2>/dev/null || echo 'fs.file-max=500000' >> /etc/sysctl.conf
 sysctl -p
-# apt-get install -y bloodhound
 neo4j start
+
+# install cypheroth, which automates bloodhound queries & outputs to CSV
+cd /opt
+git clone https://github.com/seajaysec/cypheroth
+ln -s /opt/cypheroth ~/Downloads/cypheroth
+ln -s /opt/cypheroth/cypheroth.sh /usr/local/bin/cypheroth
+
 
 
 printf '\n============================================================\n'
@@ -287,20 +297,26 @@ go get -v github.com/bettercap/bettercap
 
 
 printf '\n============================================================\n'
+printf '[+] Installing EapHammer\n'
+printf '============================================================\n\n'
+cd /opt
+git clone https://github.com/s0lst1c3/eaphammer.git
+cd eaphammer
+apt-get install $(grep -vE "^\s*#" kali-dependencies.txt  | tr "\n" " ")
+chmod +x kali-setup
+# remove prompts from setup script
+sed -i 's/.*input.*update your package list.*/    if False:/g' kali-setup
+sed -i 's/.*input.*upgrade your installed packages.*/    if False:/g' kali-setup
+sed -i 's/.*apt.* install.*//g' kali-setup
+./kali-setup
+ln -s /opt/eaphammer ~/Downloads/eaphammer
+ln -s /opt/eaphammer/eaphammer /usr/local/bin/eaphammer
+
+
+printf '\n============================================================\n'
 printf '[+] Installing Gowitness\n'
 printf '============================================================\n\n'
 go get -v github.com/sensepost/gowitness
-
-
-#printf '\n============================================================\n'
-#printf '[+] Installing Zeek\n'
-#printf '============================================================\n\n'
-#echo 'deb http://download.opensuse.org/repositories/security:/zeek/Debian_10/ /' > /etc/apt/sources.list.d/security:zeek.list
-#wget -nv https://download.opensuse.org/repositories/security:zeek/Debian_10/Release.key -O Release.key
-#apt-key add - < Release.key
-#rm Release.key
-#apt-get -y update
-#apt-get install zeek
 
 
 printf '\n============================================================\n'
@@ -313,22 +329,29 @@ git clone https://github.com/lgandx/PCredz.git
 ln -s /opt/PCredz/Pcredz.py /usr/local/bin/pcredz
 
 
+printf '\n============================================================\n'
+printf '[+] Installing EavesARP\n'
+printf '============================================================\n\n'
+cd ~/Downloads
+git clone https://github.com/arch4ngel/eavesarp
+cd eavesarp && python3 -m pip install -r requirements.txt
+cd && ln -s ~/Downloads/eavesarp/eavesarp.py /usr/local/bin/eavesarp
+
 
 printf '\n============================================================\n'
 printf '[+] Installing CrackMapExec\n'
 printf '============================================================\n\n'
 cme_dir="$(ls -d /root/.local/share/virtualenvs/* | grep CrackMapExec | head -n 1)"
 if [[ ! -z "$cme_dir" ]]; then rm -r "$cme_dir" &>/dev/null; fi
-rm -r /opt/CrackMapExec &>/dev/null
+rm -rf /opt/CrackMapExec &>/dev/null
 yes '' | apt-get install libssl-dev libffi-dev python-dev build-essential
-pip install pipenv
 cd /opt
 git clone --recursive https://github.com/byt3bl33d3r/CrackMapExec
 cd CrackMapExec && python2 -m pipenv install
 python2 -m pipenv run python setup.py install
 #ln -s ~/.local/share/virtualenvs/$(ls /root/.local/share/virtualenvs | grep CrackMapExec | head -n 1)/bin/cme /usr/bin/cme
 #ln -s ~/.local/share/virtualenvs/$(ls /root/.local/share/virtualenvs | grep CrackMapExec | head -n 1)/bin/cmedb /usr/bin/cmedb
-ln -s ~/.local/share/virtualenvs/$(ls /root/.local/share/virtualenvs | grep CrackMapExec | head -n 1)/bin ~/Downloads/crackmapexec_bleeding_edge
+ln -s ~/.local/share/virtualenvs/$(ls /root/.local/share/virtualenvs | grep CrackMapExec | head -n 1)/bin ~/Downloads/crackmapexec
 cd / && rm -r /opt/CrackMapExec
 yes '' | apt-get install crackmapexec
 
@@ -343,14 +366,14 @@ git clone https://github.com/CoreSecurity/impacket.git
 cd impacket && python2 -m pipenv install
 python2 -m pipenv run python setup.py install
 #ln -s ~/.local/share/virtualenvs/$(ls /root/.local/share/virtualenvs | grep impacket | head -n 1)/bin/*.py /usr/bin/
-ln -s ~/.local/share/virtualenvs/$(ls /root/.local/share/virtualenvs | grep impacket | head -n 1)/bin ~/Downloads/impacket_bleeding_edge
+ln -s ~/.local/share/virtualenvs/$(ls /root/.local/share/virtualenvs | grep impacket | head -n 1)/bin ~/Downloads/impacket
 cd / && rm -r /opt/impacket
 
 
 printf '\n============================================================\n'
 printf '[+] Installing Sublime Text\n'
 printf '============================================================\n\n'
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 yes '' | apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
 yes '' | apt-get update
@@ -361,7 +384,7 @@ printf '\n============================================================\n'
 printf '[+] Installing BoostNote\n'
 printf '============================================================\n\n'
 boost_deb_url="https://github.com$(curl -Ls https://github.com/BoostIO/boost-releases/releases/latest | egrep -o '/BoostIO/boost-releases/releases/download/.+.deb')"
-cd ~/Downloads
+cd /opt
 wget -O boostnote.deb "$boost_deb_url"
 yes '' | apt-get install gconf2 gvfs-bin
 dpkg -i boostnote.deb
@@ -391,9 +414,21 @@ gsettings set org.gnome.desktop.interface enable-animations false
 
 
 printf '\n============================================================\n'
+printf '[+] Disabling Terminal Transparency\n'
+printf '============================================================\n\n'
+profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
+profile=${profile:1:-1}
+gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" use-transparent-background false
+# bring back minimize/maxminize buttons
+gsettings set org.gnome.desktop.wm.preferences button-layout appmenu:minimize,maximize,close
+
+
+printf '\n============================================================\n'
 printf '[+] Enabling Tap-to-click\n'
 printf '============================================================\n\n'
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/libinput_Tapping_Enabled -s 1
+xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/libinput_Tap_Action -s 0 -s 0 -s 0 -s 0 -s 1 -s 3 -s 2
 
 
 printf '\n============================================================\n'
